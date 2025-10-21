55c06b605f69b4b1cbcd903fc5f8231d3e529b6a
marashdeh <marashdeh04@gmail.com>
2025-10-21 14:27:17 +0300

    Moved Spring Boot backend into /backend folder

---
+++ backend/collab_code_editor/src/main/java/com/example/collab_code_editor/CollabCodeEditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.collab_code_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabCodeEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabCodeEditorApplication.class, args);
+	}
+
+}
